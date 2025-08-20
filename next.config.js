38a60d1b44a2a9705e21622240ce900e4a463a8e
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-08-20 09:32:05 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Referrer-Policy', value: 'no-referrer' },
+          { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
